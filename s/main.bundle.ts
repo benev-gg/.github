1c3406390b6829b48f08c2a1e0f86bcba6402a14
Chase Moskal <chasemoskal@gmail.com>
2026-06-07 15:37:29 -0700

    use: nice new setup fn

--- s/main.bundle.ts
+++ s/main.bundle.ts
@@ -1,20 +1,8 @@
 
-import {html} from "lit"
 import {dom} from "@e280/sly"
-import {errorString} from "@e280/stz"
-import {Shell, Loader, headerComponent, menuComponent, loaderComponent} from "@benev/web"
+import {setup} from "@benev/web"
 
-const shell = new Shell()
-const loader = new Loader({
-	transitionDuration: 200,
-	errorDisplay: err => html`<div class=error>error: ${errorString(err, "unknown")}</div>`,
-})
-
-dom.register({
-	BenevHeader: headerComponent(shell),
-	BenevMenu: menuComponent(shell),
-	BenevLoader: loaderComponent(loader),
-})
+dom.register(setup().components)
 
 console.log("👼")
 
